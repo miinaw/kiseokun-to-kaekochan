5989bc04c9eaae396ac13c42c4f62f9701eb5938
miinaw <yong.abema@gmail.com>
2019-05-02 01:59:09 +0900

    add: datafile for kaekochan

---
+++ public/data/kaekochanData.js
@@ -0,0 +1,62 @@
+export const TOPS = [
+  { 
+    id: 0,
+    name: "そのまま",
+    value: 10,
+    src: "../images/girl-top.png"
+  },
+  { 
+    id: 1,
+    name: "ピンク",
+    value: 30,
+    src: "../images/girl-top_pink.png"
+  },
+  {
+    id: 2,
+    name: "きいろ",
+    value: 10,
+    src: "../images/girl-top_yellow.png"
+  },
+];
+
+export const BOTTOMS = [
+  { 
+    id: 0,
+    name: "そのまま",
+    value: 10,
+    src: "../images/girl-bottom_pink.png"
+  },
+  { 
+    id: 1,
+    name: "ピンク",
+    value: 20,
+    src: "../images/girl-bottom_pink.png"
+  },
+  {
+    id: 2,
+    name: 'チェック',
+    value: 40,
+    src: "../images/girl-bottom_check.png"
+  },
+];
+
+export const SHOES = [
+  { 
+    id: 0,
+    name: "そのまま",
+    value: 10,
+    src: "../images/girl-shoes_black.png"
+  },
+  { 
+    id: 1,
+    name: "くろ",
+    value: 20,
+    src: "../images/girl-shoes_black.png"
+  },
+  {
+    id: 2,
+    name: "ちゃいろ",
+    value: 30,
+    src: "../images/girl-shoes_brown.png"
+  },
+];
