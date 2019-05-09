076d456f6267f2413935482569d1755713ac7b30
miinaw <yong.abema@gmail.com>
2019-05-09 23:49:02 +0900

    add: kiseokunData.js

---
+++ public/data/kiseokunData.js
@@ -0,0 +1,62 @@
+export const TOPS = [
+  { 
+    id: 0,
+    name: "そのまま",
+    score: 10,
+    src: "../images/boy-top.png"
+  },
+  { 
+    id: 1,
+    name: "あお",
+    score: 30,
+    src: "../images/boy-top_blue.png"
+  },
+  {
+    id: 2,
+    name: "みどり",
+    score: 10,
+    src: "../images/boy-top_green.png"
+  },
+];
+
+export const BOTTOMS = [
+  { 
+    id: 0,
+    name: "そのまま",
+    score: 10,
+    src: "../images/boy-bottom.png"
+  },
+  { 
+    id: 1,
+    name: "あお",
+    score: 20,
+    src: "../images/boy-bottom_blue.png"
+  },
+  {
+    id: 2,
+    name: 'チェック',
+    score: 40,
+    src: "../images/boy-bottom_check.png"
+  },
+];
+
+export const SHOES = [
+  { 
+    id: 0,
+    name: "そのまま",
+    score: 30,
+    src: "../images/boy-shoes.png"
+  },
+  { 
+    id: 1,
+    name: "くろ",
+    score: 20,
+    src: "../images/boy-shoes_black.png"
+  },
+  {
+    id: 2,
+    name: "しろ",
+    score: 10,
+    src: "../images/boy-shoes_white.png"
+  },
+];
